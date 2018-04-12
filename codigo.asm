817a160e19b4381cceca6bdd047b0ea04e342c4d
bwpedro <pwarmling0@gmail.com>
2018-04-12 19:59:16 -0300

    commit antes do othon mexer

--- codigo.asm
+++ codigo.asm
@@ -2,10 +2,11 @@
 
 	entrada: .asciiz "./entrada.pgm"
 	saida:	.asciiz "saida.pgm"
-	barran: .asciiz "\n"
-	buffer_entrada: .word
-	buffer_saida: .word
-	vetor: .word 0, 0, 0
+	espaco: .asciiz " "
+	finalarquivo: .byte '0'
+	buffer_entrada: .word 0
+	buffer_saida: .word 0
+	vetor: .word 1, 2, 3
 	
 .text
 
@@ -38,151 +39,158 @@
 		addi $v0, $zero, 13    # código do syscall para abertura de arquivo
 		syscall
 		move $t1, $v0	       # salva o pointer do arquivo aberto
-		
-		
-		
-			# ler primeira e a segunda linha e salvando no arquivo
-			# fazer isso duas vezes dentro de um loop até o \n
 			
-			# ****Falta ver se o barran deu certo
-			
+		# Aqui lê a primeira, a segunda e a terceira linha do arquivo de entrada e salva no arquivo de saída
+		# ****Falta ver se o barran deu certo
 		add $s0, $zero, $zero # contadora
 		add $s1, $zero, $zero # comparação
-		addi $s2, $zero, 2 # numero de vezes do loop
-		addi $s3, $zero, 3 # end of line
+		addi $s2, $zero, 4 # numero de vezes do loop
+		addi $t7, $zero, 10 # NL line feed, new line
+		add $s4, $zero, $zero
 			
 		loopDescarte:
+			#li buffer_entrada, 0
 			slt $s1, $s0, $s2 
 			beq $s1, $zero, aqui2
 				loopFimLinha:
+				
+					#le
 					move $a0, $t0
 					la   $a1, buffer_entrada
 					addi $a2, $zero, 1
 					addi $v0, $zero, 14
 					syscall
 					add $t3, $v0, $zero
-
-					beq $a1, $s3, aqui # é o fim do arquivo?
+					
+					#escreve
+					move $a0, $t1
+					la  $a1, buffer_entrada
+					add  $a2,  $zero, $t3
+					addi $v0, $zero, 15
+					syscall
+					
+					lb $s4, buffer_entrada($zero)
+					
+					beq $s4, $t7, fimDaLinha # é o fim da linha?
+					
 					j loopFimLinha
 					
-					aqui:
-						# falta resolver: nao esta escrevendo tudo no arquivo de saida
-						move $a0, $t1
-						la  $a1, buffer_entrada
-						add  $a2,  $zero, $t3
-						addi $v0, $zero, 15
-						syscall
-			
+					fimDaLinha:
 						addi $s0, $s0, 1
 						j loopDescarte
 			
 		aqui2:
 		
-		add $s0, $zero, $zero # contadora
-		add $s1, $zero, $zero # comparação
-		add $s2, $zero, $zero # numero de vezes do loop
-		add $s3, $zero, $zero # quantidade lida
+		# Aqui os números da matriz começam a ser identificados e transformados por meio da fórmula
 		addi $s4, $zero, 32 # ascii espaço
+		la $t4, vetor # carrego o vetor em $t4
+
+		definicoesLoop:
 		
-		
-		# aqui tem que ler o tamanho do arquivo e colocar no $s2
-		
-		la $t4, matriz # provavelmente isso ta errado
+		add $s3, $zero, $zero # quantidade lida
+		addi $s5, $zero, 8 # ponteiro para a posição do vetor
+		addi $s6, $zero, 8 # constante 8
 		
 		loopMatriz:
-			slt $s1, $s0, $s2
-			beq $s1, $zero, saida
-			stringInt:
-				#to lendo 1 por 1 pq não sei mais o que fazer
-				move $a0, $t0            # a0 recebe o arquivo aberto
-				la   $a1, buffer_entrada # a1 recebe o buffer de entrada
-				addi $a2, $zero, 1   # a2 recebe o tamanho maximo
-				addi $v0, $zero, 14	 # syscall para ler do arquivo
-				syscall
-				add $t3, $v0, $zero # quantidade caracteres lidos
-				
-				lb $a1, buffer_entrada($zero) # carregando o primeiro byte do buffer para a1
-				bne $a1, $s4, naoSoma1
-				addi $s3, $s3, 1
-				naoSoma1:
-					addi $a1, $a1, -48
-					sw $a1,8($t4) # colocando a1 na terceira posição do vetor
-				
-				addi $s3, $s3, 4
-				lb $a1, buffer_entrada($s3) # carregando o segundo byte do buffer para a1
-				bne $a1, $s4, naoSoma2
-				addi $s3, $s3, 1
-				
-				j vaiPraCa
-				naoSoma2:
-					addi $a1, $a1, -48
-					sw $a1,4($t4) # colocando a1 na segunda posição do vetor
+			# Lê o caracter
+			move $a0, $t0            # a0 recebe o arquivo aberto
+			la   $a1, buffer_entrada # a1 recebe o buffer de entrada
+			addi $a2, $zero, 1   # a2 recebe o tamanho maximo
+			addi $v0, $zero, 14	 # syscall para ler do arquivo
+			syscall
+			add $t3, $v0, $zero # quantidade caracteres lidos
+			beq $t3, $zero, exit # é o fim do arquivo?
 					
-				vaiPraCa:
+			# Transforma o caracter lido em inteiro
+			lb $a1, buffer_entrada($zero)
+			beq $a1, $s4, ehEspaco # é um espaço?
+			beq $a1, $t7, ehEspaco # é \n? 
+			addi $s3, $s3, 1
+			addi $a1, $a1, -48
+
+			
+			# Insiro o caracter lido na posição do vetor
+			add $s5, $t4, $s5
+			sw $a1,0($s5)
+			addi $s6, $s6, -4
+
+			add $s5, $s6, $zero
+			
+			
+			
+			j loopMatriz
+			
+			# Se for um espaço, então o número inteiro já foi lido
+			ehEspaco:
 				
-				addi $s3, $s3, 4
-				lb $a1, buffer_entrada($s3) # carregando o terceiro byte do buffer para a1
-				bne $a1, $s4, naoSoma3
-				addi $s3, $s3, 1
-				naoSoma3:
-					addi $a1, $a1, -48
-					sw $a1,0($t4) # colocando a1 na primeira posição do vetor
-				
-				
-				addi $s4, $zero, 2 # variavel contadora
+				addi $s0, $zero, 1 # constante 1
+				addi $s1, $zero, 2 # constante 2
+				addi $s2, $zero, 3 # constante 3
+				sub $s4, $s2, $s3 # loop começa com esse valor (3-quantidadeLida)
 				add $s5, $zero, $zero # flag de comparacao
-				addi $s3, $s3, -3
-				add $s5, $zero, $zero
+				#addi $s3, $s3, 3
+				add $s6, $zero, $zero #lugar onde vai ser carregado as posições do vetor
+				add $s7, $zero, $zero # cont
 				
-				# falta termina aqui
-				somaPosicoes:
-					slt $s5, $s3, $s4
-					beq $s5, $zero, exit
-						beq
+				add $t5, $zero, $zero # soma de tudo (x da fórmula)
+				addi $t6, $zero, 255 # constante 255
+				
+			somaPosicoes:
+				beq $s2, $s4, formula
+				beq $s7, $zero, mult1
+				beq $s7, $s0, mult10
+				beq $s7, $s1, mult100
+				
+						
+					mult1:
 						lw $s6, 0($t4)
+						#mul $s6, $s6, 1
+						add $t5, $t5, $s6
+						addi $s4, $s4, 1
+						addi $s7, $s7, 1
+						j somaPosicoes
+						
+					mult10:
+						lw $s6, 4($t4)
+						mul $s6, $s6, 10
+						add $t5, $t5, $s6
+						addi $s4, $s4, 1
+						addi $s7, $s7, 1
+						j somaPosicoes
+						
+					mult100:
+						lw $s6, 8($t4)
+						mul $s6, $s6, 100
+						add $t5, $t5, $s6
+						addi $s4, $s4, 1
+						addi $s7, $s7, 1
+						j somaPosicoes
+						
+			
+				formula:
+					sub $t5, $t6, $t5
+					addi $t5, $t5, 48
+					
+
+			
+			# Aqui o valor obtido na fórmula é passado para o buffer e é escrito no arquivo de saída	
+			sw $t5, buffer_saida # move o buffer para o $t5
+			
+			add $a0, $zero, $t5
+			addi $v0, $zero, 1
+			syscall
 			
 			
-			# transformar string em inteiro 
-				# salvar no vetor com o numero ao contrario, de trás pra frente, ex se for 253 salvar 352, usar loop
-				# usando lb (load byte) contando os espaços (olhar a folha)
-				# a partir disso subtrair 48 de cada numero encontrado
-				# multiplicar a primeira posição por 1
-				# multiplicar a segunda posição por 10
-				# multiplicar a terceira posição por 100
-				# ir somando em um registrador ex t4
-		
-				#ex:
-				# lb $t4, buffer_entrada($zero) # carregando um byte do buffer
-				# addi $t4, $t4, -48 # transforma a string em um numero
-		
-			# ler no maximo 3 caracteres por vez, pois no maximo 255
+			move $a0, $t1	# descriptor que está em t1 vem para a0
+			la  $a1, buffer_saida  # buffer de saida é o mesmo
+			add  $a2,  $zero, $t3	 # a leitura (syscall) 14 retorna a quantidade de caracteres em v0. Precisa ser passado aqui
+			addi $v0, $zero, 15	 # syscall para escrever no arquivo
+			syscall
 			
-				# le arquivo
+			add $t5, $zero, $zero
 			
-				move $a0, $t0            # a0 recebe o arquivo aberto
-				la   $a1, buffer_entrada # a1 recebe o buffer de entrada
-				addi $a2, $zero, 3   # a2 recebe o tamanho maximo
+			j definicoesLoop
 			
-				# transformar string em inteiro (processo da folha) usando lb
-			
-				addi $v0, $zero, 14	 # syscall para ler do arquivo
-				syscall
-				add $t3, $v0, $zero # quantidade caracteres lidos
-				
-				# aplicar a formula: soma = -soma+255
-				# levar a soma para o buffer
-		
-				# escreve 
-				
-				move $a0, $t1		 # descriptor que está em t1 vem para a0
-				la  $a1, buffer_saida  # buffer de saida é o mesmo
-				add  $a2,  $zero, $t3	 # a leitura (syscall) 14 retorna a quantidade de caracteres em v0. Precisa ser passado aqui
-				addi $v0, $zero, 15	 # syscall para escrever no arquivo
-				syscall
-			
-				addi $s0, $s0, 1
-				j loopMatriz
-	
 		# desempilha registradores
 		sw $a2, 0($sp)
 		sw $a1, 4($sp)
@@ -204,6 +212,11 @@
 		
 	
 	exit:
-		addi $v0, $zero, 10  # syscall para encerrar o programa
+		move $a0, $t1
+		la $a1, finalarquivo
+		addi $a2, $zero, 1
+		addi $v0, $zero, 15
 		syscall
 		
+		addi $v0, $zero, 10  # syscall para encerrar o programa
+		syscall
